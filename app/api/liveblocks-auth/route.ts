ced104385c757720fec25951e8a64ce811d21791
i0li <ioli.sa1013@gmail.com>
2024-12-25 18:37:25 +0900

    modify readme & add liveblock auth api

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -7,8 +7,7 @@ import { api } from "@/convex/_generated/api";
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 const liveblocks = new Liveblocks({
-  secret:
-    "sk_dev_tAnZ88ulbRoY_WxnQ32xAiaE_ciEH0cjlht8TJWDOhdfQNDOaxmOObQxsmiidCZc",
+  secret: process.env.NEXT_PUBLIC_LIVEBLOCKS_SECRET_KEY!,
 });
 
 export async function POST(request: Request) {
